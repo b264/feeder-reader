992ae668d647caaaa9e53fde7b5b523da3f375ff
theB264 <public88878878887@hotmail.com>
2014-10-16 17:54:21 -0500

    create channel

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -3,45 +3,42 @@ class ChannelsController < ApplicationController
   # GET /channels.json
   def index
     @channels = Channel.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @channels }
     end
   end
-
   # GET /channels/1
   # GET /channels/1.json
   def show
     @channel = Channel.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @channel }
     end
   end
-
   # GET /channels/new
   # GET /channels/new.json
   def new
-    @channel = Channel.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @channel }
+    unless valid_url?
+      get_url
+    end
+    if valid_url?
+      fetch_new_channel
+      clear_url
+    else
+      #TODO flash message about bad link
+      index
     end
   end
-
   # GET /channels/1/edit
   def edit
     @channel = Channel.find(params[:id])
   end
-
   # POST /channels
   # POST /channels.json
   def create
     @channel = Channel.new(params[:channel])
-
     respond_to do |format|
       if @channel.save
         format.html { redirect_to @channel, notice: 'Channel was successfully created.' }
@@ -52,12 +49,10 @@ class ChannelsController < ApplicationController
       end
     end
   end
-
   # PUT /channels/1
   # PUT /channels/1.json
   def update
     @channel = Channel.find(params[:id])
-
     respond_to do |format|
       if @channel.update_attributes(params[:channel])
         format.html { redirect_to @channel, notice: 'Channel was successfully updated.' }
@@ -68,16 +63,34 @@ class ChannelsController < ApplicationController
       end
     end
   end
-
   # DELETE /channels/1
   # DELETE /channels/1.json
   def destroy
     @channel = Channel.find(params[:id])
     @channel.destroy
-
     respond_to do |format|
       format.html { redirect_to channels_url }
       format.json { head :no_content }
     end
   end
+  private
+  def valid_url?
+    @link
+  end
+  def clear_url
+    @link= false
+  end
+  def get_url
+    #TODO
+    @link= 'http://feeds.bbci.co.uk/news/rss.xml'
+  end
+  def fetch_new_channel
+      @channel = Channel.new
+      #TODO
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @channel }
+      end
+  end
+  
 end
--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,2 +1,5 @@
 module ChannelsHelper
+  require 'open-uri'              # allows open('http://...') to return body
+  require 'cgi'                   # for escaping URIs
+  require 'nokogiri'              # XML parser
 end
