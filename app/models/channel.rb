8547c983110fab760d3f16c28efd8af5dc10b4df
theB264 <public88878878887@hotmail.com>
2014-10-17 08:54:38 -0500

    item creation/destruction

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,10 +5,15 @@ class Channel < ActiveRecord::Base
                   :webMaster
   validate :validate_save
   
-  has_many :items
+  has_many :items, :dependent => :destroy
   
   def validate_url url
     url && connect_to(url)
+  end
+  def attrs
+    list= Channel.accessible_attributes
+    list.delete ''
+    list
   end
   
   private
@@ -26,26 +31,36 @@ class Channel < ActiveRecord::Base
     end
   end
   def update_channel
-    #begin
+    begin
       xml= open(self.URL).read
       puts xml #TODO: remove this
       doc= Nokogiri::XML(xml) {|config| config.strict.nonet}
-      parse doc, Channel.accessible_attributes
-    #rescue #Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError => er
-      #false
-    #end
+      parse doc, attrs << 'item'
+    rescue #Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError => er
+      false
+    end
   end
   def parse doc, fields
+    #TODO: refactor
     fields.each do |attr|
       unless ['URL', ''].include? attr.to_s
-        if self.methods.include? attr.to_sym
-          unless doc.xpath("//#{attr.to_s}").empty?
-            case attr.to_s
-              when 'item'
-                
-              else
-                #TODO: security hole, code execution
-                eval "self.#{attr.to_s}= doc.xpath('//#{attr.to_s}').first.content"
+        if attr.to_s== 'item'
+          doc.xpath('//item').each do |item_node|
+            item= self.items.new
+            item_node.children.each do |node|
+              unless [''].include? node.name.to_s
+                if item.methods.include? node.name.to_sym
+                  eval "item.#{node.name.to_s}= node.text"
+                end
+              end
+            end
+            item.save
+          end
+        else
+          #TODO: refactor
+          if self.methods.include? attr.to_sym
+            unless doc.xpath("//#{attr.to_s}").empty?
+              eval "self.#{attr.to_s}= doc.xpath('//#{attr.to_s}').first.content"
             end
           end
         end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,12 @@ class Item < ActiveRecord::Base
   attr_accessible :title, :link, :description, :channel_id, :author, :category_id,
                   :comments, :enclosure, :guid, :pubDate, :source
   
-  belongs_to :channel
+  belongs_to :channel, :inverse_of => :item
+  
+  def attrs
+    list= Item.accessible_attributes
+    list.delete ''
+    list
+  end
   
 end
